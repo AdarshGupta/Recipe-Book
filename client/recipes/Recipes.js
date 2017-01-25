a84f11ed83c7f2288484a153539c9a5ffd93711c
AdarshGupta <adarsh035@gmail.com>
2017-01-25 12:46:29 +0530

    Added CSS and displayed recipes in the collection

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,3 +1,19 @@
 Meteor.subscribe('recipes');
 
 console.log(Meteor.settings.public.ga.account);
+
+Template.Recipes.helpers({
+	recipes: function () {	// Can be replace by ()=> {}  which means This is the new ES2015 Arrow Function syntax. Essentially, you use it to preserve "this". () => { } accomplishes a function just as function() {} would, but passes the current 'this' into the function
+		// ...
+		return Recipes.find({}, {
+			/*
+			sort: Sort specifier,
+			skip: Number,
+			limit: Number,
+			fields: Field specifier,
+			reactive: Boolean,
+			transform: Function
+			*/
+		});
+	}
+});
